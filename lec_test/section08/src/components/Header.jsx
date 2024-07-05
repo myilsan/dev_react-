1540bf7d20b963ca31e59b02ca238b7ebe0653fb
myilsan <168319312+myilsan@users.noreply.github.com>
2024-07-05 09:48:39 +0900

    Update Header.jsx

--- lec_test/section08/src/components/Header.jsx
+++ lec_test/section08/src/components/Header.jsx
@@ -2,7 +2,7 @@ import "./Header.css";
 const Header=()=>{
     return (
     <div className="Header">
-        <h3>오늘은👩🏾‍🤝‍👩🏽</h3>
+        <h3>오늘은 👩🏾‍🤝‍👩🏽</h3>
         <h1>{new Date().toDateString()}</h1>
 
     </div>
