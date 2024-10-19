ef8c829c670c743c306d9a0c4e06c0965c8d06c0
myilsan <myilsan72@naver.com>
2024-10-19 23:23:07 +0900

    스크립트 테스트

---
+++ lec_test/section01/chapter12.js
@@ -0,0 +1,70 @@
+let varC = (value)=> {
+
+    console.log(value);
+    return value +1;
+}
+
+varC(10);
+
+function main(value){
+    console.log(1);
+    console.log(2);
+    console.log(3);
+    console.log(4);
+    console.log(5);
+    value();
+    
+}
+
+function sub()
+{
+    console.log("i am sub");
+}
+
+main(sub)
+
+
+// 1.Spread 연산자
+//--> Spread : 흩뿌리다, 펼치다 라는 뜻
+//--<
+
+let  arr =[1,2,3];
+let  [a1, a2, a3] = arr;
+console.log(a1, a2, a3);
+
+
+let arr1 =[1,2,3];
+let arr2 =[4, ...arr1,5,6];
+
+console.log(arr2);
+
+for(let i=0; i < arr.length; i++){
+    console.log(arr[i]);
+}
+
+arr2.forEach(element => {
+    arr(element);
+});
+
+for (const element of arr2) {
+    arr(element);
+}
+
+//객체 for문 사용하기 
+let person ={
+    name : "최경호",
+    age: 25,
+    hobby : "등산" 
+}
+
+let keys = Object.keys(person);
+for(let key of keys)
+{
+    const value = person[key];
+    console.log(key,value);
+}
+
+for (let key in arr2) {
+    const value = person[key]
+    console.log(key, value);
+}
