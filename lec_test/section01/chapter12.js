c0e7a0c83c19b92cc9dea7b208977a46758c86a6
myilsan <myilsan72@naver.com>
2024-10-20 23:15:36 +0900

    반영

--- lec_test/section01/chapter12.js
+++ lec_test/section01/chapter12.js
@@ -12,10 +12,7 @@ function main(value) {
     console.log(3);
     console.log(4);
     console.log(5);
-
-
     value();
-
 }
 
 function sub() {
@@ -68,3 +65,107 @@ for (let key in person) {
     const value = person[key]
     console.log(key, value);
 }
+
+
+const mapResult = arr2.map((item,idx, arr)=>{    
+    return item * 2 ;    
+});
+
+console.log(mapResult);
+
+setTimeout(()=>{
+    console.log("콜백 응답");
+},1000);
+
+
+//***비동기 콜백 ***/
+function add(a ,b, callback){
+    setTimeout(()=>{
+        const sum = a+ b;
+        callback(sum);
+    },2000);
+}
+
+add(1, 45 , (result)=>{
+    //console.log(result);
+})
+
+/*********************/
+
+
+//** promise  test **/
+console.log(" promise  test");
+function add10(num)
+{
+    const promise = new Promise((resulve,reject)=>{ //resulve : 성공일때 , reject:실패일때
+        setTimeout(()=>{
+            if(typeof num ==="number"){
+                resulve(num +10);
+            }else{
+                reject("not number");
+            }
+        },2000)
+    });
+    return promise;
+}
+
+add10(0)
+.then((result)=>{
+    console.log(result);    
+    const newP = add10(result);   
+    return newP;
+})
+.then((result)=>{
+    console.log(result);
+    return add10(result)
+})
+.then((result)=>{
+    console.log(result);
+    return add10(10);
+})
+.catch((error)=>{
+    console.log(error);
+})
+
+//setTimeout(()=>{
+//   console.log(promise);
+//},3000)
+
+//성공일때
+//promise.then((value)=>{
+//    console.log(value);    
+
+//}).catch((error)=>{  //실패일때 
+    //console.log(error);
+//});
+
+
+//실패
+//promise.catch((error)=>{
+//   console.log(error);
+//});
+
+
+async function getData(){
+    const data = new Promise((resulve,reject)=>{ //resulve : 성공일때 , reject:실패일때
+        setTimeout(()=>{            
+            resulve({
+                name:"최경호",
+                id:"myilsan"
+            });           
+        },4000);
+    });
+
+    return data;
+}
+
+
+
+async function printData()
+{
+    //await getData().then((result)=>{console.log(result)});
+    const data = await getData();
+    console.log(data);
+}
+
+printData();
