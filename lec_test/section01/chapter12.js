b771b99398a471afbf54d4e43e5fa6f0d5fb500a
최경호 <myilsan72@nave.com>
2024-10-24 18:02:46 +0900

    수정

--- lec_test/section01/chapter12.js
+++ lec_test/section01/chapter12.js
@@ -167,6 +167,39 @@ async function printData()
 }
 
 
-
-
 printData();
+
+
+// 0,null,undefined,"" => 은 falsy 임 나머지 는 모두  truthy 임
+
+ 
+/******************************
+ * && (AND) 연산자
+*******************************/
+// // 첫 번째 피연산자가 truthy이면,
+// // AND는 두 번째 피연산자를 반환합니다.
+// alert( 1 && 0 ); // 0
+// alert( 1 && 5 ); // 5
+
+// // 첫 번째 피연산자가 falsy이면,
+// // AND는 첫 번째 피연산자를 반환하고, 두 번째 피연산자는 무시합니다.
+// alert( null && 5 ); // null
+// alert( 0 && "아무거나 와도 상관없습니다." ); // 0
+
+//alert( 1 && 2 && null && 3 ); // null
+//alert( 1 && 2 && 3 ); // 마지막 값, 3
+
+
+/******************************
+ * || (OR) 연산자
+*******************************/
+//alert( 1 || 0 ); // 1 (1은 truthy임)
+//alert( null || 1 ); // 1 (1은 truthy임)
+//alert( null || 0 || 1 ); // 1 (1은 truthy임)
+//alert( undefined || null || 0 ); // 0 (모두 falsy이므로, 마지막 값을 반환함)
+
+//alert 는 undefined 으로 응답한다.
+
+//alert( alert(1) && alert(2) && 3 )
+alert( "" && alert(2) && 5 );
+
