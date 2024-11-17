2c9d8ec8a1e83d823669f31e95ac6c6941752d82
myilsan <myilsan72@naver.com>
2024-11-17 23:46:27 +0900

    변경적용

---
+++ lec_test/section12/src/component/Button.jsx
@@ -0,0 +1,8 @@
+import "./Button.css";
+export default function Button({ text, type, onClick }) {
+  return (
+    <button onClick={onClick} className={`Button Button_${type}`}>
+      {text}
+    </button>
+  );
+}
