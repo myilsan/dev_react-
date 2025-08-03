dc74371f73a5005a282da6cbddc1e877b11ac094
myilsan <myilsan72@naver.com>
2025-08-03 19:58:11 +0900

    develop 수정

--- lec_test/jstest/index.js
+++ lec_test/jstest/index.js
@@ -37,7 +37,7 @@ getData(
  * 5. 수정 반영해주세요Promise 객체의 catch() 메소드를 호출하여 에러 처리
  * 4. Promise 객체의 then() 메소드의 콜백 함수에서 Propumise 객체의 resolve() 메소드의 인자로 전달한 값에 접근
  * 6. devalop에서 수정
- * 
+ */
 function getData2(param1, param2) {
   return new Promise((resolve) => {
     setTimeout(() => {
