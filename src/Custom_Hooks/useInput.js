ac9a02cd33bbd254b44271962b7b8a78589cd91e
myilsan <myilsan72@naver.com>
2025-08-03 23:46:14 +0900

    수정

--- src/Custom_Hooks/useInput.js
+++ src/Custom_Hooks/useInput.js
@@ -1,19 +1,17 @@
 import { useState } from "react";
 
-export function useInput(initValue)
-{
-    //Custm hooks  
-    const  [inputValue , setInputValue_] = useState(initValue);  
+export function useInput(initValue) {
+  //Custm hooks
+  const [inputValue, setInputValue_] = useState(initValue);
 
-    const handleChange = (e)=>{
-      //console.log(e);
-      setInputValue_(e.target.value);
-    };
+  const handleChange = (e) => {
+    //console.log(e);
+    setInputValue_(e.target.value);
+  };
 
-    const setInputValue =(e)=>
-    {
-        setInputValue_(e);
-    };
-    //callback
-    return [inputValue,setInputValue, handleChange,]
+  const setInputValue = (e) => {
+    setInputValue_(e);
+  };
+  //callback 입니다.
+  return [inputValue, setInputValue, handleChange];
 }
